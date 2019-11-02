83fae3f9bbc80385a1b50e5f0a6adcae30f54629
github username <github email address>
2019-11-02 18:17:32 +0000

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,52 @@
 class Startup
 
+    @@all = []
+    attr_reader :founder, :domain
+    attr_accessor :name
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pivot(new_domain, new_name)
+        @domain = new_domain
+        @name = new_name
+    end
+
+    def self.find_by_founder(sought_founder)
+        self.all.find{|startup| startup.founder == sought_founder}
+    end
+
+    def self.domains
+        self.all.map{|s| s.domain}.uniq
+    end
+
+    def sign_contract(venture_capitalist, type, investment)
+        FundingRound.new(self, venture_capitalist,type, investment)
+    end
+
+    def num_funding_rounds
+        FundingRound.all.count{|fr| fr.startup == self}
+    end
+
+    def funding_rounds
+        FundingRound.all.select{|fr| fr.startup == self}
+    end
+
+    def investors
+        funding_rounds.map{|fr| fr.venture_capitalist}.uniq
+    end
+
+    def big_investors
+        investors.select{|i| i.total_worth > 1_000_000_000}
+    end
+
+  
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,41 @@
 class VentureCapitalist
 
+    @@all = []
+    attr_reader :name
+    attr_accessor :total_worth
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_comma_club
+        self.all.select{|vc| vc.total_worth > 1_000_000_000}
+    end
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup,self,type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select{|fr| fr.venture_capitalist == self}
+    end
+
+    def portfolio
+        funding_rounds.map{|fr| fr.startup}
+    end
+
+    def biggest_investment
+        funding_rounds.map{|fr| fr.investment}.max
+    end
+
+    def invested(sought_domain)
+        funding_rounds.select{|fr| fr.domain == sought_domain}.map{|fr| fr.investment}.sum
+    end
+
 end
