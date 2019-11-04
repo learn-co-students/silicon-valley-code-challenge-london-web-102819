1cedbe1e914dbf41488356a025ac4cdac9f2ef4f
andrew-lewell <andrew.lewell@gmail.com>
2019-11-04 17:01:36 +0000

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,44 @@
 class VentureCapitalist
 
+    attr_reader :name, :total_worth
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self 
+    end 
+
+    def self.all
+        @@all 
+    end 
+
+    def self.tres_commas_club
+        self.all.select { |vc| vc.total_worth > 1000000000 }
+    end 
+
+    def offer_contract(startup, type, amount)
+        FundingRound.new(startup, self, type, amount)
+    end 
+
+    def funding_rounds 
+        FundingRound.all.select { |fr| fr.venture_capitalist == self }
+    end 
+
+    def portfolio
+        funding_rounds.map { |fr| fr.startup }.uniq
+    end 
+
+    def biggest_investment 
+        funding_rounds.max_by { |fr| fr.investment }
+    end 
+
+    def invested(domain)
+        #binding.pry
+        total = 0 
+        funding_rounds.each { |fr| total += fr.investment if fr.startup.domain == domain }
+        total
+    end 
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,21 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+s1 = Startup.new("Monzo", "John Monzo", "FinTech")
+s2 = Startup.new("Uber", "Jack Uber", "Transport")
+s3 = Startup.new("Flatiron", "Mr Flatiron", "Education")
+
+vc1 = VentureCapitalist.new("Elon Musk", 1200000000)
+vc2 = VentureCapitalist.new("Ashton Kutcher", 50000000)
+vc3 = VentureCapitalist.new("Joe Bloggs", 1200000)
+
+s1.sign_contract(vc1, "Angel", 50000.00)
+s2.sign_contract(vc1, "Angel", 100000.00)
+s1.sign_contract(vc3, "Angel", 123000.00)
+s1.sign_contract(vc2, "Seed 2", 60000.00)
+
+vc3.offer_contract(s3, "Seed 1", 10000.00)
+vc2.offer_contract(s1, "Angel", 20000.00)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
