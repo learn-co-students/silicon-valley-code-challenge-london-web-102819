aa07f9ab3443d5dd0ba0b527ae4a4234f99ad5e3
Qing <chingwang.qw@gmail.com>
2019-11-04 17:47:37 +0000

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,81 @@
+require "pry"
 class Startup
+  attr_reader :name, :founder, :domain
 
+  @@all = []
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pivot(domain)
+    @domain = domain
+  end 
+
+  #given a string of a **founder's name**,
+  # returns the **first startup** whose founder's name matches
+  def self.find_by_founder(founder_name)
+    self.all.find {|s| s.founder == founder_name}
+  end
+
+  #should return an **array** of all of the different startup domains
+  def self.domains
+    self.all.map {|s| s.domain}
+  end
+
+  #given a **venture capitalist object**, type of investment (as a string), and the amount invested (as a float), creates a new funding round and associates 
+  #it with that startup and venture capitalist.
+  #(startup, venture_capitalist, type，investment)
+  def sign_contract(venture_capitalist_object, investment_type, investment_amount)
+    FundingRound.new(self, venture_capitalist_object, investment_type, investment_amount)
+  end
+
+  def fundings
+    FundingRound.all.select {|f| f.startup == self}
+  end
+
+  #Returns the total number of funding rounds that the startup has gotten
+  def num_funding_rounds
+    fundings.size
+  end
+
+  #Returns the total sum of investments that the startup has gotten
+  def total_funds
+    (fundings.map {|f| f.investment}).sum
+  end
+
+  def investors
+    fundings.map {|fr| fr.venture_capitalist}.uniq
+  end
+
+
+  #Returns a **unique** array of all the venture capitalists that have invested in 
+  #this company and are in the Trés Commas club
+  def big_investors
+    tres_commas_club_investor = VentureCapitalist.tres_commas_club
+    big_investor = tres_commas_club_investor & investors
+  end
 end
+
+puts 0
+
+=begin
+
+ Startup#sign_contract`
+  - given a **venture capitalist object**, type of investment (as a string), and the amount invested (as a float), creates a new funding round and associates it with that startup and venture capitalist.
+- `Startup#num_funding_rounds`
+  - Returns the total number of funding rounds that the startup has gotten
+- `Startup#total_funds`
+  - Returns the total sum of investments that the startup has gotten
+- `Startup#investors`
+  - Returns a **unique** array of all the venture capitalists that have invested in this company
+- `Startup#big_investors`
+  - Returns a **unique** array of all the venture capitalists that have invested in this company and are in the Trés Commas club
+  
+=end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,29 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+##### Startup Class Testing ############
+#(name, founder, domain)
+s1 = Startup.new("PWC", "Qing", "account")
+s2 = Startup.new("Flatiron", "Avi", "Programming Education")
+s3 = Startup.new("Waitrose", "Tom", "Supermarket")
+
+##### Venture_capitalist Class Testing ############
+#(name, total_worth)
+v1= VentureCapitalist.new("venture 1", 7000000)
+v2= VentureCapitalist.new("venture 2", 2000000000)
+v3= VentureCapitalist.new("venture 3", 800000000)
+v4= VentureCapitalist.new("venture 4", 10000000001)
+
+##### Founding_round Class Testing ############
+#f1 =(startup, venture_capitalist, type)
+f1 = FundingRound.new(s1, v1, "Series A", 2000)
+f2 = FundingRound.new(s2, v2, "Series B", 3000)
+f3 = FundingRound.new(s3, v3, "Series C", 4000)
+f4 = FundingRound.new(s1, v4, "Series D", 50000)
+f5 = FundingRound.new(s2, v2, "Series E", 500000)
+f6 = FundingRound.new(s1, v2, "Series F", 1090000)
+f7 = FundingRound.new(s2, v3, "Series G", 1090000)
+f8 = FundingRound.new(s1, v2, "Series I", 80000)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
